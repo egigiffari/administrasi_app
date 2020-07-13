4291d00526439e1e5415f69f72024eaa029321b9
egigiffari <egisipayung3@gmail.com>
2020-07-13 18:01:49 +0700

    Pindah Duedate Ke Atas

--- resources/views/request/export/pdf/biaya.blade.php
+++ resources/views/request/export/pdf/biaya.blade.php
@@ -57,6 +57,11 @@
             <div class="subject" style="padding-left: 10px;padding-top: 10px;">
                 <table>
                     <tr>
+                        <td width='45px'>Due Date</td>
+                        <td width="5px">:</td>
+                        <td>{{ date('g F Y', strtotime($request->expire_date)) }}</td>
+                    </tr>
+                    <tr>
                         <td width='45px'>Tanggal</td>
                         <td width="5px">:</td>
                         <td>{{ date('g F Y', strtotime($request->start_date)) }}</td>
@@ -71,11 +76,7 @@
                         <td width="5px">:</td>
                         <td>{{ $request->code }}</td>
                     </tr>
-                    <tr>
-                        <td width='45px'>Due Date</td>
-                        <td width="5px">:</td>
-                        <td>{{ date('g F Y', strtotime($request->expire_date)) }}</td>
-                    </tr>
+                    
                 </table>
             </div>
 
